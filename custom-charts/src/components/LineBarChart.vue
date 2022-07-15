6f7b4af5290918d6e3950c1dd4cf69358a087a45
karimorozova <morozova.karine13@gmail.com>
2022-07-15 10:25:36 +0300

    Create LineBarChart.vue

---
+++ custom-charts/src/components/LineBarChart.vue
@@ -0,0 +1,74 @@
+<template lang="pug">
+.lineBarChart
+    .label(v-if="isLabels")
+      .label__text {{ series[0] }}{{ symbol }}
+      .label__text(style="opacity: .8") /
+      .label__text {{ series[1] }}{{ symbol }}
+
+    .lineBar(
+      :style="{'height': height + 'px', 'width': width  + 'px'}"
+    )
+      .lineBar__range( :style="{'background': colors[0], 'width': series[0] + '%' }" )
+      .lineBar__splitter(v-if="isSplitter")
+      .lineBar__range( :style="{'background': colors[1], 'width': series[1] + '%' }" )
+
+</template>
+
+<script>
+
+export default {
+  name: "LineBarChart",
+
+  props: {
+    height: {
+      type: Number,
+      default: 7
+    },
+    width: {
+      type: Number,
+      default: 140
+    },
+    series: {
+      type: Array,
+      default: () => [ 50, 50 ]
+    },
+    colors: {
+      type: Array,
+      default: () => [ '#d66f58', '#4ba5a5'] 
+    },
+    symbol: {
+      type: String,
+      default: "%"
+    },
+    isLabels: {
+      type: Boolean,
+      default: true
+    },
+    isSplitter: {
+      type: Boolean,
+      default: true
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+.lineBar {
+  display: flex;
+
+  &__splitter {
+    width: 2px;
+    background: transparent;
+  }
+}
+
+.label {
+  display: flex;
+  justify-content: center;
+  gap: 4px;
+  font-size: 12px;
+  color: gray;
+  margin-bottom: 4px;
+}
+</style>
