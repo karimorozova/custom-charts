0106ba6fd68bb1f41782edc91a256737abecfe1a
karimorozova <morozova.karine13@gmail.com>
2022-07-15 10:25:14 +0300

    Create Custom Chart app

---
+++ custom-charts/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <DonutChart/>
+  <LineBarChart />
+  <PieChart/>
+   <SemiDonutChart/>
+</template>
+
+<script>
+import DonutChart from './components/DonutChart.vue'
+import LineBarChart from './components/LineBarChart.vue'
+import PieChart from './components/PieChart.vue'
+import SemiDonutChart from './components/SemiDonutChart.vue'
+
+export default {
+  name: 'App',
+  components: {
+    DonutChart,
+    LineBarChart,
+    PieChart,
+    SemiDonutChart
+}
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
